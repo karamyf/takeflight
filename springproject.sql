bf6ad479cafd35488a23f4d9f3e530d8b7be56b9
karamyf <karam_youssef@emsi-edu.ma>
2023-05-04 12:29:02 +0100

    user update + errors fixed

--- springproject.sql
+++ springproject.sql
@@ -1,11 +1,12 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 10, 2022 at 06:02 PM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 8.0.2
+-- Generation Time: May 04, 2023 at 12:45 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+USE springproject;
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,7 +31,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `categories` (
   `categoryid` int(11) NOT NULL,
   `name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `categories`
@@ -51,7 +52,7 @@ CREATE TABLE `login` (
   `id` int(11) NOT NULL,
   `password` varchar(20) NOT NULL,
   `username` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `login`
@@ -59,6 +60,37 @@ CREATE TABLE `login` (
 
 INSERT INTO `login` (`id`, `password`, `username`) VALUES
 (1, '123', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payments`
+--
+
+CREATE TABLE `payments` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `card_number` varchar(16) NOT NULL,
+  `expiry_date` date NOT NULL,
+  `cvc` varchar(3) NOT NULL,
+  `street_address` varchar(100) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `zip_code` varchar(10) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `payments`
+--
+
+INSERT INTO `payments` (`id`, `first_name`, `last_name`, `card_number`, `expiry_date`, `cvc`, `street_address`, `city`, `zip_code`, `email`, `created_at`) VALUES
+(1, 'youssef', 'sadiqy', '321681', '2023-12-01', '123', 'dazedazd', 'casa', '22646', 'test@gmail.com', '2023-04-29 17:48:41'),
+(2, 'youssef', 'sadiqy', '321681', '2023-12-01', '123', 'dazedazd', 'casa', '22646', 'test@gmail.com', '2023-04-29 17:52:51'),
+(3, 'youssef', 'sadiqy', '6541864', '2023-12-01', '123', 'dazd', 'test', '22646', 'tattoostore@falltrack.net', '2023-04-29 18:07:02'),
+(4, 'aaa', 'aaa', '6548/465', '2023-12-01', '841', '133dzad81z', 'test', '13254', 'test@gmail.com', '2023-05-04 10:35:45'),
+(5, 'baaaa', 'sadiqy', '4451561', '2012-12-01', '213', 'dzadzafdzf', 'fze', '22646', 'kousouf@protonmail.com', '2023-05-04 10:40:01');
 
 -- --------------------------------------------------------
 
@@ -75,16 +107,17 @@ CREATE TABLE `products` (
   `price` int(11) NOT NULL,
   `weight` int(11) NOT NULL,
   `description` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `products`
 --
 
 INSERT INTO `products` (`id`, `name`, `image`, `categoryid`, `quantity`, `price`, `weight`, `description`) VALUES
-(14, 'dfgdg', '1.jpg', 6, 7275275, 24, 27, ''),
-(15, 'dfgdg', '2.jpg', 6, 7275275, 24, 27, ''),
-(16, 'apple', '', 11, 5, 30, 10, 'red python');
+(14, 'Philippines', 'https://www.voyagesinsolites.com/wp-content/uploads/vacances-aux-Philippines-300x300.jpg', 6, 165, 14329, 27, ''),
+(15, 'Argentine', 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/28/9d/1e/cb/caption.jpg?w=300&h=300&s=1', 6, 265, 12229, 27, ''),
+(16, 'Morocco', 'https://www.immobilier-pro-maroc.com/wp-content/uploads/2018/10/mercado-jamaa-el-fna-marrakesh-marrocos-1-300x300.jpg', 11, 346, 7999, 10, 'morocco africa'),
+(17, 'Korea', 'https://www.georgeclinical.com/wp-content/uploads/2017/03/south-korea-sm-300x300.jpeg', 6, 359, 16599, 0, '');
 
 -- --------------------------------------------------------
 
@@ -96,10 +129,10 @@ CREATE TABLE `users` (
   `user_id` int(11) NOT NULL,
   `username` varchar(45) NOT NULL,
   `password` varchar(64) NOT NULL,
-  `role` varchar(45) NOT NULL,
+  `role` varchar(250) NOT NULL DEFAULT 'ROLE_USERS',
   `enabled` tinyint(4) DEFAULT NULL,
   `email` varchar(110) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
@@ -107,7 +140,9 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`user_id`, `username`, `password`, `role`, `enabled`, `email`) VALUES
 (1, 'jay', '123', 'ROLE_USER', 1, 'gajerajay9@gmail.com'),
-(2, 'admin', '123', 'ROLE_ADMIN', 1, '20ceuos042@ddu.ac.in');
+(2, 'admin', '123', 'ROLE_ADMIN', 1, '20ceuos042@ddu.ac.in'),
+(3, 'test', 'Test@123', 'ROLE_USERS', NULL, 'tes@gmail.com'),
+(4, 'test', 'Test@123', 'ROLE_USERS', NULL, 'tests@gmail.com');
 
 --
 -- Indexes for dumped tables
@@ -118,6 +153,12 @@ INSERT INTO `users` (`user_id`, `username`, `password`, `role`, `enabled`, `emai
 --
 ALTER TABLE `categories`
   ADD PRIMARY KEY (`categoryid`);
+
+--
+-- Indexes for table `payments`
+--
+ALTER TABLE `payments`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `products`
@@ -143,16 +184,22 @@ ALTER TABLE `categories`
   MODIFY `categoryid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
+-- AUTO_INCREMENT for table `payments`
+--
+ALTER TABLE `payments`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
@@ -164,11 +211,6 @@ ALTER TABLE `users`
 ALTER TABLE `products`
   ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`categoryid`) REFERENCES `categories` (`categoryid`) ON DELETE CASCADE;
 COMMIT;
-
-ALTER TABLE `users` 
-CHANGE COLUMN `role` `role` VARCHAR(250) NULL;
-
-ALTER TABLE `users` CHANGE COLUMN `role` `role` VARCHAR(250) NOT NULL DEFAULT 'ROLE_USERS';
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- src/main/java/com/jtspringproject/JtSpringProject/controller/AdminController.java
+++ src/main/java/com/jtspringproject/JtSpringProject/controller/AdminController.java
@@ -18,9 +18,9 @@ public class AdminController {
 		usernameforclass = "";
 		return "userLogin";
 	}
-	
-	
-	
+
+
+
 	@GetMapping("/index")
 	public String index(Model model) {
 		if(usernameforclass.equalsIgnoreCase(""))
@@ -29,46 +29,46 @@ public class AdminController {
 			model.addAttribute("username", usernameforclass);
 			return "index";
 		}
-			
+
 	}
 	@GetMapping("/userloginvalidate")
 	public String userlog(Model model) {
-		
+
 		return "userLogin";
 	}
 	@RequestMapping(value = "userloginvalidate", method = RequestMethod.POST)
 	public String userlogin( @RequestParam("username") String username, @RequestParam("password") String pass,Model model) {
-		
+
 		try
 		{
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/springproject","root","");
 			Statement stmt = con.createStatement();
 			ResultSet rst = stmt.executeQuery("select * from users where username = '"+username+"' and password = '"+ pass+"' ;");
 			if(rst.next()) {
 				usernameforclass = rst.getString(2);
 				return "redirect:/index";
-				}
+			}
 			else {
 				model.addAttribute("message", "Invalid Username or Password");
 				return "userLogin";
 			}
-			
+
 		}
 		catch(Exception e)
 		{
 			System.out.println("Exception:"+e);
 		}
 		return "userLogin";
-		
-		
-		
+
+
+
 	}
-	
-	
+
+
 	@GetMapping("/admin")
 	public String adminlogin(Model model) {
-		
+
 		return "adminlogin";
 	}
 	@GetMapping("/adminhome")
@@ -80,16 +80,16 @@ public class AdminController {
 	}
 	@GetMapping("/loginvalidate")
 	public String adminlog(Model model) {
-		
+
 		return "adminlogin";
 	}
 	@RequestMapping(value = "loginvalidate", method = RequestMethod.POST)
 	public String adminlogin( @RequestParam("username") String username, @RequestParam("password") String pass,Model model) {
-		
+
 		if(username.equalsIgnoreCase("admin") && pass.equalsIgnoreCase("123")) {
 			adminlogcheck=1;
 			return "redirect:/adminhome";
-			}
+		}
 		else {
 			model.addAttribute("message", "Invalid Username or Password");
 			return "adminlogin";
@@ -104,14 +104,14 @@ public class AdminController {
 	{
 		try
 		{
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/springproject","root","");
 			Statement stmt = con.createStatement();
-			
+
 			PreparedStatement pst = con.prepareStatement("insert into categories(name) values(?);");
 			pst.setString(1,catname);
 			int i = pst.executeUpdate();
-			
+
 		}
 		catch(Exception e)
 		{
@@ -119,20 +119,20 @@ public class AdminController {
 		}
 		return "redirect:/admin/categories";
 	}
-	
+
 	@GetMapping("/admin/categories/delete")
 	public String removeCategoryDb(@RequestParam("id") int id)
 	{
 		try
 		{
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/springproject","root","");
 			Statement stmt = con.createStatement();
-			
+
 			PreparedStatement pst = con.prepareStatement("delete from categories where categoryid = ? ;");
 			pst.setInt(1, id);
 			int i = pst.executeUpdate();
-			
+
 		}
 		catch(Exception e)
 		{
@@ -140,21 +140,21 @@ public class AdminController {
 		}
 		return "redirect:/admin/categories";
 	}
-	
+
 	@GetMapping("/admin/categories/update")
 	public String updateCategoryDb(@RequestParam("categoryid") int id, @RequestParam("categoryname") String categoryname)
 	{
 		try
 		{
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/springproject","root","");
 			Statement stmt = con.createStatement();
-			
+
 			PreparedStatement pst = con.prepareStatement("update categories set name = ? where categoryid = ?");
 			pst.setString(1, categoryname);
 			pst.setInt(2, id);
 			int i = pst.executeUpdate();
-			
+
 		}
 		catch(Exception e)
 		{
@@ -178,34 +178,34 @@ public class AdminController {
 		int pid,pprice,pweight,pquantity,pcategory;
 		try
 		{
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/springproject","root","");
 			Statement stmt = con.createStatement();
 			Statement stmt2 = con.createStatement();
 			ResultSet rst = stmt.executeQuery("select * from products where id = "+id+";");
-			
+
 			if(rst.next())
 			{
-			pid = rst.getInt(1);
-			pname = rst.getString(2);
-			pimage = rst.getString(3);
-			pcategory = rst.getInt(4);
-			pquantity = rst.getInt(5);
-			pprice =  rst.getInt(6);
-			pweight =  rst.getInt(7);
-			pdescription = rst.getString(8);
-			model.addAttribute("pid",pid);
-			model.addAttribute("pname",pname);
-			model.addAttribute("pimage",pimage);
-			ResultSet rst2 = stmt.executeQuery("select * from categories where categoryid = "+pcategory+";");
-			if(rst2.next())
-			{
-				model.addAttribute("pcategory",rst2.getString(2));
-			}
-			model.addAttribute("pquantity",pquantity);
-			model.addAttribute("pprice",pprice);
-			model.addAttribute("pweight",pweight);
-			model.addAttribute("pdescription",pdescription);
+				pid = rst.getInt(1);
+				pname = rst.getString(2);
+				pimage = rst.getString(3);
+				pcategory = rst.getInt(4);
+				pquantity = rst.getInt(5);
+				pprice =  rst.getInt(6);
+				pweight =  rst.getInt(7);
+				pdescription = rst.getString(8);
+				model.addAttribute("pid",pid);
+				model.addAttribute("pname",pname);
+				model.addAttribute("pimage",pimage);
+				ResultSet rst2 = stmt.executeQuery("select * from categories where categoryid = "+pcategory+";");
+				if(rst2.next())
+				{
+					model.addAttribute("pcategory",rst2.getString(2));
+				}
+				model.addAttribute("pquantity",pquantity);
+				model.addAttribute("pprice",pprice);
+				model.addAttribute("pweight",pweight);
+				model.addAttribute("pdescription",pdescription);
 			}
 		}
 		catch(Exception e)
@@ -215,14 +215,14 @@ public class AdminController {
 		return "productsUpdate";
 	}
 	@RequestMapping(value = "admin/products/updateData",method=RequestMethod.POST)
-	public String updateproducttodb(@RequestParam("id") int id,@RequestParam("name") String name, @RequestParam("price") int price, @RequestParam("weight") int weight, @RequestParam("quantity") int quantity, @RequestParam("description") String description, @RequestParam("productImage") String picture ) 
-	
+	public String updateproducttodb(@RequestParam("id") int id,@RequestParam("name") String name, @RequestParam("price") int price, @RequestParam("weight") int weight, @RequestParam("quantity") int quantity, @RequestParam("description") String description, @RequestParam("productImage") String picture )
+
 	{
 		try
 		{
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/springproject","root","");
-			
+
 			PreparedStatement pst = con.prepareStatement("update products set name= ?,image = ?,quantity = ?, price = ?, weight = ?,description = ? where id = ?;");
 			pst.setString(1, name);
 			pst.setString(2, picture);
@@ -231,7 +231,7 @@ public class AdminController {
 			pst.setInt(5, weight);
 			pst.setString(6, description);
 			pst.setInt(7, id);
-			int i = pst.executeUpdate();			
+			int i = pst.executeUpdate();
 		}
 		catch(Exception e)
 		{
@@ -239,20 +239,20 @@ public class AdminController {
 		}
 		return "redirect:/admin/products";
 	}
-	
+
 	@GetMapping("/admin/products/delete")
 	public String removeProductDb(@RequestParam("id") int id)
 	{
 		try
 		{
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/springproject","root","");
-			
-			
+
+
 			PreparedStatement pst = con.prepareStatement("delete from products where id = ? ;");
 			pst.setInt(1, id);
 			int i = pst.executeUpdate();
-			
+
 		}
 		catch(Exception e)
 		{
@@ -260,14 +260,14 @@ public class AdminController {
 		}
 		return "redirect:/admin/products";
 	}
-	
+
 	@PostMapping("/admin/products")
 	public String postproduct() {
 		return "redirect:/admin/categories";
 	}
 	@RequestMapping(value = "admin/products/sendData",method=RequestMethod.POST)
 	public String addproducttodb(@RequestParam("name") String name, @RequestParam("categoryid") String catid, @RequestParam("price") int price, @RequestParam("weight") int weight, @RequestParam("quantity") int quantity, @RequestParam("description") String description, @RequestParam("productImage") String picture ) {
-		
+
 		try
 		{
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/springproject","root","");
@@ -275,17 +275,17 @@ public class AdminController {
 			ResultSet rs = stmt.executeQuery("select * from categories where name = '"+catid+"';");
 			if(rs.next())
 			{
-			int categoryid = rs.getInt(1);
-			
-			PreparedStatement pst = con.prepareStatement("insert into products(name,image,categoryid,quantity,price,weight,description) values(?,?,?,?,?,?,?);");
-			pst.setString(1,name);
-			pst.setString(2, picture);
-			pst.setInt(3, categoryid);
-			pst.setInt(4, quantity);
-			pst.setInt(5, price);
-			pst.setInt(6, weight);
-			pst.setString(7, description);
-			int i = pst.executeUpdate();
+				int categoryid = rs.getInt(1);
+
+				PreparedStatement pst = con.prepareStatement("insert into products(name,image,categoryid,quantity,price,weight,description) values(?,?,?,?,?,?,?);");
+				pst.setString(1,name);
+				pst.setString(2, picture);
+				pst.setInt(3, categoryid);
+				pst.setInt(4, quantity);
+				pst.setInt(5, price);
+				pst.setInt(6, weight);
+				pst.setString(7, description);
+				int i = pst.executeUpdate();
 			}
 		}
 		catch(Exception e)
@@ -294,35 +294,35 @@ public class AdminController {
 		}
 		return "redirect:/admin/products";
 	}
-	
+
 	@GetMapping("/admin/customers")
 	public String getCustomerDetail() {
 		return "displayCustomers";
 	}
-	
-	
+
+
 	@GetMapping("profileDisplay")
 	public String profileDisplay(Model model) {
 		String displayusername,displaypassword,displayemail,displayaddress;
 		try
 		{
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/springproject","root","");
 			Statement stmt = con.createStatement();
 			ResultSet rst = stmt.executeQuery("select * from users where username = '"+usernameforclass+"';");
-			
+
 			if(rst.next())
 			{
-			int userid = rst.getInt(1);
-			displayusername = rst.getString(2);
-			displayemail = rst.getString(3);
-			displaypassword = rst.getString(4);
-			displayaddress = rst.getString(5);
-			model.addAttribute("userid",userid);
-			model.addAttribute("username",displayusername);
-			model.addAttribute("email",displayemail);
-			model.addAttribute("password",displaypassword);
-			model.addAttribute("address",displayaddress);
+				int userid = rst.getInt(1);
+				displayusername = rst.getString(2);
+				displayemail = rst.getString(3);
+				displaypassword = rst.getString(4);
+				displayaddress = rst.getString(5);
+				model.addAttribute("userid",userid);
+				model.addAttribute("username",displayusername);
+				model.addAttribute("email",displayemail);
+				model.addAttribute("password",displaypassword);
+				model.addAttribute("address",displayaddress);
 			}
 		}
 		catch(Exception e)
@@ -332,23 +332,23 @@ public class AdminController {
 		System.out.println("Hello");
 		return "updateProfile";
 	}
-	
+
 	@RequestMapping(value = "updateuser",method=RequestMethod.POST)
-	public String updateUserProfile(@RequestParam("userid") int userid,@RequestParam("username") String username, @RequestParam("email") String email, @RequestParam("password") String password, @RequestParam("address") String address) 
-	
+	public String updateUserProfile(@RequestParam("userid") int userid,@RequestParam("username") String username, @RequestParam("email") String email, @RequestParam("password") String password, @RequestParam("address") String address)
+
 	{
 		try
 		{
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/springproject","root","");
-			
+
 			PreparedStatement pst = con.prepareStatement("update users set username= ?,email = ?,password= ?, address= ? where uid = ?;");
 			pst.setString(1, username);
 			pst.setString(2, email);
 			pst.setString(3, password);
 			pst.setString(4, address);
 			pst.setInt(5, userid);
-			int i = pst.executeUpdate();	
+			int i = pst.executeUpdate();
 			usernameforclass = username;
 		}
 		catch(Exception e)
