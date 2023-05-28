9ba91f20b1bb99586be418eda473db238020767f
karamyf <karam_youssef@emsi-edu.ma>
2023-05-28 18:55:53 +0100

    payment process signature

--- src/main/java/com/jtspringproject/JtSpringProject/controller/UserController.java
+++ src/main/java/com/jtspringproject/JtSpringProject/controller/UserController.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class UserController{
 
+
 	@GetMapping("/register")
 	public String registerUser()
 	{
@@ -61,70 +62,41 @@ public class UserController{
 		}
 		return "redirect:/";
 	}
+
+
 	@GetMapping("/add_review")
 	public String showAddReviewForm(Model model) {
-		// Add any necessary model attributes here
 		return "add_review";
 	}
-
 	@PostMapping("/add_review")
 	public String addReview(@RequestParam("name") String name, @RequestParam("rating") int rating,
 							@RequestParam("image") MultipartFile image, @RequestParam("text") String text) {
 
 		try {
-			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/springproject","root","");
-			PreparedStatement pst = conn.prepareStatement("insert into reviews(name, rating, image, text) values(?,?,?,?);");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/springproject", "root", "");
+			PreparedStatement pst = conn.prepareStatement("INSERT INTO reviews (name, rating, image, text) VALUES (?, ?, ?, ?);");
+
 			pst.setString(1, name);
 			pst.setInt(2, rating);
-			if(image != null && !image.isEmpty()) {
+			if (image != null && !image.isEmpty()) {
 				pst.setBinaryStream(3, image.getInputStream());
 			} else {
 				pst.setNull(3, Types.BLOB);
 			}
 			pst.setString(4, text);
+
 			pst.executeUpdate();
-			System.out.println("database updated");
+			System.out.println("Database updated");
+
 			conn.close();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			System.out.println("Exception: " + e);
 		}
 		return "redirect:/index";
 	}
 
 
-	@PostMapping("/process-payment")
-	public String processPayment(@RequestParam("first-name") String firstName, @RequestParam("last-name") String lastName, @RequestParam String number,
-								 @RequestParam String expiry, @RequestParam String cvc, @RequestParam String streetaddress,
-								 @RequestParam String city, @RequestParam String zipcode, @RequestParam String email) {
-		try {
-			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/springproject","root","");
-			PreparedStatement pst = conn.prepareStatement("insert into payments(first_name, last_name, card_number, expiry_date, cvc, street_address, city, zip_code, email) values(?,?,?,?,?,?,?,?,?);");
 
-			// Convert expiry date to MySQL date format
-			SimpleDateFormat format1 = new SimpleDateFormat("MM/yy");
-			SimpleDateFormat format2 = new SimpleDateFormat("yyyy-MM-dd");
-			Date expiryDate = format1.parse(expiry);
-			String expiryDateStr = format2.format(expiryDate);
-
-			pst.setString(1, firstName);
-			pst.setString(2, lastName);
-			pst.setString(3, number);
-			pst.setString(4, expiryDateStr);
-			pst.setString(5, cvc);
-			pst.setString(6, streetaddress);
-			pst.setString(7, city);
-			pst.setString(8, zipcode);
-			pst.setString(9, email);
-
-
-			int i = pst.executeUpdate();
-			System.out.println("database updated"+i);
-
-		} catch(Exception e) {
-			System.out.println("Exception: " + e);
-		}
-		return "redirect:/thankyou";
-	}
 
 
 }
--- src/main/webapp/views/categories.jsp
+++ src/main/webapp/views/categories.jsp
@@ -1,5 +1,6 @@
 <!doctype html>
 <%@page import="java.sql.*"%>
+<%@ include file="header.jsp" %>
 
 <html lang="en" xmlns:th="http://www.thymeleaf.org">
 <head>
@@ -18,32 +19,7 @@
 <title>Document</title>
 </head>
 <body class="bg-light">
-	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-		<div class="container-fluid">
-			<a class="navbar-brand" href="#"> <img src="@{/images/logo.png}"
-				src="../static/images/logo.png" width="auto" height="40"
-				class="d-inline-block align-top" alt="" />
-			</a>
-			<button class="navbar-toggler" type="button" data-toggle="collapse"
-				data-target="#navbarSupportedContent"
-				aria-controls="navbarSupportedContent" aria-expanded="false"
-				aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-
-			<div class="collapse navbar-collapse" id="navbarSupportedContent">
-				<ul class="navbar-nav mr-auto"></ul>
-				<ul class="navbar-nav">
-					<li class="nav-item active"><a class="nav-link" href="/adminhome">Home
-							Page</a></li>
-					<li class="nav-item active"><a class="nav-link" href="/logout">Logout</a>
-					</li>
-
-				</ul>
-
-			</div>
-		</div>
-	</nav><br>
+	<br>
 	<div class="container">
 
 
